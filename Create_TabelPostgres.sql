0bd7c04ade2624c85dcf6d69f54ed628d7b147bf
jorge <luisortiz211994@gmail.com>
2025-02-05 11:36:10 -0500

    feature insert

--- Create_TabelPostgres.sql
+++ Create_TabelPostgres.sql
@@ -1,5 +1,14 @@
 --Create table in postgres
-create table users_roles(
+create table users.users_roles(
  id serial not NULL,
  name VARCHAR(100) NOT NULL
 );
+
+---get users
+select * from users.users u 
+
+select * from users.users_roles ur 
+
+INSERT INTO users.users
+(id_usr, name_user)
+VALUES(nextval('users.users_id_usr_seq'::regclass), 'jorge');
