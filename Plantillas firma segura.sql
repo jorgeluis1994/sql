50e0ea13a7c09b3c8c6673a6279e480643fc5213
jorge <luisortiz211994@gmail.com>
2025-02-07 09:37:56 -0500

    feature platilla

--- Plantillas firma segura.sql
+++ Plantillas firma segura.sql
@@ -1,9 +1,26 @@
 --ESTRUCTURA DE PANTALLAS FIRMA DIGITAL
+
+--alianza sp.id_establishment =9
 select
 	*
 from
 	structure_process sp
+	where sp.id_establishment =9
 	
 --DETALLE DE ESTRUCTURA
 	
-select * from detail_struc_proc dsp 
+select
+	*
+from
+	detail_struc_proc dsp
+where
+	dsp.id_struc_proc in(35, 36)
+	
+
+--DOCUEMTOS 
+select
+	*
+from
+	"document" d
+where
+	d.id_establishment = 9
