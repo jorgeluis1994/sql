e6a82c8271b50244f868a22e3d2ce7f8881f00c7
jorge <luisortiz211994@gmail.com>
2025-02-07 10:26:25 -0500

    feature firma view

--- Plantillas firma segura.sql
+++ Plantillas firma segura.sql
@@ -1,11 +1,13 @@
 --ESTRUCTURA DE PANTALLAS FIRMA DIGITAL
 
---alianza sp.id_establishment =9
+select * from establishment e 
+
+--Seguros Alianza = 9 Sweaden Seguros = 8
 select
 	*
 from
 	structure_process sp
-	where sp.id_establishment =9
+	where sp.id_establishment =8
 	
 --DETALLE DE ESTRUCTURA
 	
@@ -14,7 +16,7 @@ select
 from
 	detail_struc_proc dsp
 where
-	dsp.id_struc_proc in(35, 36)
+	dsp.id_struc_proc in(9,8)
 	
 
 --DOCUEMTOS 
@@ -24,3 +26,31 @@ from
 	"document" d
 where
 	d.id_establishment = 9
+	
+--DETALLE DE DOUMENTOS
+	
+select
+	count(*)
+from
+	detail_document dd
+where
+	dd.id_det_strpro in(8)
+
+select
+	*
+from
+	detail_document dd
+where
+	dd.id_det_strpro in(9)
+	
+--Vista de documentos
+	
+select
+	*
+from
+	document_view dv
+where
+	dv.id_establishment = 8
+	
+
+
